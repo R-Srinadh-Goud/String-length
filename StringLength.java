8b30bb7ea2e39d14fda8d6534171fca5efdb13aa
R-Srinadh-Goud <java19012025@gmail.com>
2025-01-31 00:24:25 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "R.Srinadh Goud.";
+        int length = str.length();
+        System.out.println("Length: " + length);   
+    }
+}
+
